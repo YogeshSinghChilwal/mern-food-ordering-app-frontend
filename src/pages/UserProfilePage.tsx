a5e5c74b805b8d161b690a838deb31c58ad718ca
Yogesh Singh Chilwal <yogeshsinghchilwal@gmail.com>
2024-11-14 18:52:23 +0530

    updated UserProfilePage

--- src/pages/UserProfilePage.tsx
+++ src/pages/UserProfilePage.tsx
@@ -1,6 +1,6 @@
 import { useGetMyUser, useUpdateMyUser } from '@/api/MyUserApi'
 import UserProfileForm from '@/forms/user-profile-form/UserProfileForm'
-import React from 'react'
+
 
 export default function UserProfilePage() {
 
