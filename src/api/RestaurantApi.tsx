56eb045d0169c8ceab3711ba69ca01e2330487c0
Yogesh Singh Chilwal <yogeshsinghchilwal@gmail.com>
2024-12-19 17:15:10 +0530

    added cart details

--- src/api/RestaurantApi.tsx
+++ src/api/RestaurantApi.tsx
@@ -1,8 +1,24 @@
 import { SearchState } from "@/pages/SearchPage";
-import { RestaurantSearchResponse } from "@/types";
+import { Restaurant, RestaurantSearchResponse } from "@/types";
 import { useQuery } from "react-query";
 
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+
+export const useGetRestaurant = (restaurantId?: string) => {  // id is optional because the first time this hook loads we might not have the restaurantId on the first render since we are getting that from the usePrams hook
+  const getRestaurantByIdRequest = async (): Promise<Restaurant> => {
+    const response = await fetch(`${API_BASE_URL}/api/restaurant/${restaurantId}`)
+
+    if(!response){
+      throw new Error("Failed to get restaurant")
+    }
+
+    return response.json()
+  }
+
+  const {data: restaurant, isLoading} = useQuery("fetchRestaurant", getRestaurantByIdRequest, {enabled: !!restaurantId}) //* telling useQuery to only call function if there is restaurantId
+
+  return {restaurant, isLoading}
+}
 
 export const useSearchRestaurants = (
   searchState: SearchState,
